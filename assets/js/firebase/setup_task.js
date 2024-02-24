2cb12ce970a999006047956a30ce9edc8f9c5db2
ivannia <ivannid.n.c@gmail.com>
2024-02-24 12:25:10 -0500

    diana33

--- assets/js/firebase/setup_task.js
+++ assets/js/firebase/setup_task.js
@@ -7,18 +7,20 @@ let id = "";
 let editStatus = false;
 let userGlobal;
 
-  export default function setupTask(user)  {
-    userGlobal=user;
+export default function setupTask(user) {
+    userGlobal = user;
     console.log(user);
     onGetTask((querySnapshot) => {
         let html = '';
 
-        // READ
         querySnapshot.forEach(doc => {
             const data = doc.data();
 
+            const numLines = data.description.split('\n').length;
+            const backgroundSize = `calc(${numLines * 24}px + 624px)`; // Ajusta 20px por línea y agrega 200px como tamaño mínimo
+
             html += `
-                <div class="card mb-3">
+                <div class="card mb-3" style="background-size: ${backgroundSize};">
                     <div class="card-body">
                         <h6> ${data.userName}</h6>
                         <h5> ${data.date}</h5>
@@ -26,8 +28,8 @@ let userGlobal;
                         <h4 class="card-title">${data.title}</h4>
                         <p class="card-text">${data.description}</p>
                         <div class="row">
-                            <button class='btn btn-danger btn-delete-custom mx-auto col-5' data-id='${doc.id}'>Delete</button>
-                            <button class='btn btn-info btn-edit-custom mx-auto col-5' data-id='${doc.id}'>Edit</button>
+                            <button class='btn btn-rojo btn-delete-custom mx-auto col-5' data-id='${doc.id}'>Delete</button>
+                            <button class='btn btn-celeste btn-edit-custom mx-auto col-5' data-id='${doc.id}'>Edit</button>
                         </div>
                     </div>
                 </div>
@@ -36,18 +38,16 @@ let userGlobal;
 
         tasksContainer.innerHTML = html;
 
-        // DELETE
         const btnsDelete = document.querySelectorAll(".btn-delete-custom");
 
         btnsDelete.forEach(btn => {
-            btn.addEventListener("click", ({target: { dataset }}) => deleteTask(dataset.id));
+            btn.addEventListener("click", ({ target: { dataset } }) => deleteTask(dataset.id));
         });
 
-        //UPDATE
         const btnsEdit = document.querySelectorAll(".btn-edit-custom");
 
         btnsEdit.forEach(btn => {
-            btn.addEventListener("click", async ({target: {dataset}}) => {
+            btn.addEventListener("click", async ({ target: { dataset } }) => {
                 const doc = await getTask(dataset.id);
                 const task = doc.data();
 
@@ -57,41 +57,33 @@ let userGlobal;
                 editStatus = true;
                 id = doc.id;
 
-                taskForm['btn-task-save'].innerHTML = 'actualizar'
-                //taskText.innerHTML = 'EditTask';
-
+                taskForm['btn-task-save'].innerHTML = 'actualizar';
             });
         });
     });
-};
+}
 
-// CREATE
 taskForm.addEventListener("submit", (e) => {
-    // Evitamos que recargue la pagina
     e.preventDefault();
 
-
-    const fullDate =new Date();
-    const date=getFormattedDate(fullDate);
-    const time= getFormattedHour(fullDate);
-    const userName =userGlobal.displayName;
+    const fullDate = new Date();
+    const date = getFormattedDate(fullDate);
+    const time = getFormattedHour(fullDate);
+    const userName = userGlobal.displayName;
 
     const title = taskForm["task-title"].value;
     const description = taskForm["task-content"].value;
-  // si no estoy editando el boton sirve para crear 
-    if (!editStatus){
-        createTask(title, description,userName, date,time);
-    }
-    else {
+
+    if (!editStatus) {
+        createTask(title, description, userName, date, time);
+    } else {
         updateTask(id, ({
-            title:title,
-            description:description
+            title: title,
+            description: description
         }));
         editStatus = false;
 
-        taskForm['btn-task-save'].innerHTML = 'escribe'
-         //taskText.innerHTML = 'New Task';
-
+        taskForm['btn-task-save'].innerHTML = 'escribe';
     }
 
     taskForm.reset();
@@ -99,30 +91,13 @@ taskForm.addEventListener("submit", (e) => {
 
 function getFormattedDate(date) {
     var year = date.getFullYear();
-  
-    var month = (1 + date.getMonth()).toString();
-    month = month.length > 1 ? month : '0' + month;
-  
-    var day = date.getDate().toString();
-    day = day.length > 1 ? day : '0' + day;
-    
-    return month + '/' + day + '/' + year;
+    var month = (1 + date.getMonth()).toString().padStart(2, '0');
+    var day = date.getDate().toString().padStart(2, '0');
+    return `${month}/${day}/${year}`;
+}
 
-    
-  }
-
-
-  function getFormattedHour(date){
-    var hour =date.getHours();
-    var minutes =date.getMinutes();
-
-    if (hour < 10){
-        hour ='0'+hour;
-    }
-
-    if (minutes < 10){
-        minutes ='0'+minutes;
-    }
-    
-    return hour +':' + minutes;
-  }
+function getFormattedHour(date) {
+    var hour = date.getHours().toString().padStart(2, '0');
+    var minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hour}:${minutes}`;
+}
